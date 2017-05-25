1adec5d8c684f8f83841156cc6d86ed602a4099a
Darren Riley <darrenriley@Darrens-MacBook-Pro.local>
2017-05-24 20:14:55 -0400

    add scroll and blog link section

---
+++ js/vul.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+
+
+});
